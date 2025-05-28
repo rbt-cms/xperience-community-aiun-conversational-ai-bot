206edb76f08b50379d08577df0cfae426cf71d2e
Vasu <vasu.yerramsetti@raybiztech.com>
2025-05-28 08:42:57 +0530

    Initial code commit and resolved the following bug ''Tag' is an ambiguous reference between 'CMS.ContentEngine.Tag' and 'HotChocolate.Types.Tag'' with  adding this import using Tag = CMS.ContentEngine.Tag;

---
+++ tests/XperienceCommunity.AIUN.ConversationalAIBot.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace XperienceCommunity.AIUN.ConversationalAIBot.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
